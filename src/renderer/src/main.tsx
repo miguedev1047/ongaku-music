58bf559f8bc14d76730a131b39e27aaededa3c2f
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-08 04:23:13 -0500

    FEAT: setup project ⛏️

---
+++ src/renderer/src/main.tsx
@@ -0,0 +1,36 @@
+import '@/styles/main.css'
+
+import { scan } from 'react-scan'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+import { queryClient } from '@/services/react-query'
+
+scan({ enabled: import.meta.env.DEV })
+
+const hashHistory = createHashHistory()
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  history: hashHistory,
+  context: { queryClient },
+  defaultPreload: 'intent',
+  scrollRestoration: true
+})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<RouterProvider router={router} />)
+}
