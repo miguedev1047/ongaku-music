02fb7786ef9fb719ea1b51ab76913871284cc83c
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-08 04:31:42 -0500

    FEAT: add general constants ⚙️

---
+++ src/renderer/src/constants/general.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_URL_IMG =
+  'https://cdn.iconscout.com/icon/free/png-512/free-music-icon-svg-download-png-439129.png?f=webp&w=256'
+
+export const APP_NAME = 'Ongaku Music'
+
+export const DOWNLOAD_OPTS = [
+  { label: 'Only Song', value: 'only-song' },
+  { label: 'Only Playlist', value: 'only-playlist' }
+]
