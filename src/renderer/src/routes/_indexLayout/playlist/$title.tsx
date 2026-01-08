93c3bb09090cf92e21fc9e87ed6dad0001a3a8a8
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-08 04:34:24 -0500

    FEAT: add playlist/title route 📄

---
+++ src/renderer/src/routes/_indexLayout/playlist/$title.tsx
@@ -0,0 +1,25 @@
+import { SongList } from '@/components/songs/_index'
+import { playlistSongsQueryOpts } from '@/queries/playlists-queries'
+import { createFileRoute } from '@tanstack/react-router'
+import { Suspense } from 'react'
+
+export const Route = createFileRoute('/_indexLayout/playlist/$title')({
+  component: RouteComponent,
+  loader: async ({ params, context }) => {
+    await context.queryClient.ensureQueryData(playlistSongsQueryOpts(params.title))
+    return params
+  }
+})
+
+function RouteComponent() {
+  const { title } = Route.useLoaderData()
+
+  return (
+    <section className="w-full h-full flex flex-col flex-1 gap-4">
+      <h2 className="font-bold uppercase">{title}</h2>
+      <Suspense fallback={<p>Loading...</p>}>
+        <SongList />
+      </Suspense>
+    </section>
+  )
+}
