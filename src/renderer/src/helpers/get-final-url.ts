6072a202793b942e7bc7578df6457e173085efbc
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-09 04:49:48 -0500

    CHORE: fix the playlist url error 🔨

--- src/renderer/src/helpers/get-final-url.ts
+++ src/renderer/src/helpers/get-final-url.ts
@@ -3,10 +3,10 @@ interface Props {
   url: string
 }
 
-export function getFinalUrl(props: Props) {
+export function getFinalUrl(props: Props): string {
   const { option, url } = props
   const normalUrl = url.split('&').slice(0, 1).join('')
-  const playlistUrl = url.split('&').slice(0, 2).join('')
+  const playlistUrl = url.split('&').slice(0, 2).join('&')
   if (option === 'only-song') return normalUrl
   if (option === 'only-playlist') return playlistUrl
   return normalUrl
