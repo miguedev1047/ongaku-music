dc588992f7883d8fef166c105928626d8d478796
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-12 10:54:24 -0500

    CHORE: update toaster position ⚙️

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -4,21 +4,20 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { QueryClient } from '@tanstack/react-query'
 import { Suspense } from 'react'
 import { Toaster } from '@/components/ui/sonner'
+import { NotFound } from '@/components/router/not-found'
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient
 }>()({
   component: RootLayout,
-  notFoundComponent: () => {
-    return <div>Not found</div>
-  }
+  notFoundComponent: NotFound
 })
 
 function RootLayout() {
   return (
     <Providers>
       <Outlet />
-      <Toaster />
+      <Toaster position="top-center" />
       <Suspense>
         <TanstackDevtools />
       </Suspense>
