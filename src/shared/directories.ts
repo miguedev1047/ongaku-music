de32e5cb99722653ed638d610ce68cbac1d78241
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-12 07:26:05 -0500

    FEAT: add directories constants 🔨

---
+++ src/shared/directories.ts
@@ -0,0 +1,9 @@
+import { homedir, tmpdir } from 'node:os'
+import { join } from 'node:path'
+
+// App Directories
+export const HOME_DIR = homedir()
+export const APP_DIR = join(HOME_DIR, 'Music', 'Ongaku')
+export const PLAYLIST_DIR = join(APP_DIR, 'Playlists')
+export const TEMP_DIR = join(tmpdir(), 'ongaku-cache')
+export const DEFAULT_PLAYLIST_DIR = join(HOME_DIR, 'Music', 'Ongaku', 'Playlists', 'Default')
