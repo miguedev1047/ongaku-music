de9390ac733915df286d395125d2ee5330f982d0
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-12 07:25:49 -0500

    CHORE: add react query toast error & stale time 🔨

--- src/renderer/src/services/react-query.ts
+++ src/renderer/src/services/react-query.ts
@@ -1,7 +1,22 @@
 import { QueryCache, QueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
-    onError: () => {}
-  })
+    onError: () => {
+      toast.error('An ocurred a error', {
+        action: {
+          label: 'Retry',
+          onClick: () => {
+            queryClient.invalidateQueries()
+          }
+        }
+      })
+    }
+  }),
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 60 * 15
+    }
+  }
 })
