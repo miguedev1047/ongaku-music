0da933516219669bd688825dfe6d9022876801fd
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-13 15:13:06 -0500

    STYLE: remove margin bottom 🎨

--- src/renderer/src/routes/_indexLayout/index.tsx
+++ src/renderer/src/routes/_indexLayout/index.tsx
@@ -10,7 +10,7 @@ export const Route = createFileRoute('/_indexLayout/')({
 
 function Index() {
   return (
-    <SectionContainer className="mb-8">
+    <SectionContainer>
       <DownloadInfo />
 
       <div className="space-y-4">
