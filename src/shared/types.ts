845656afaf063c75292a1604a53ab6dc19d928d2
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-13 15:04:27 -0500

    FEAT: add song actions types 🔨

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,7 +4,10 @@ import type {
   SongModel,
   DownloadSongModel,
   WatchPlaylistModel,
-  ResponseModel
+  ResponseModel,
+  MoveSongModel,
+  RenameSongModel,
+  RemoveSongModel
 } from './models'
 
 export type GetPlaylists = (filename: string) => Promise<PlaylistModel[]>
@@ -15,6 +18,10 @@ export type NewPlaylist = (playlistName: string) => Promise<ResponseModel>
 export type RemovePlaylist = (playlistName: string) => Promise<ResponseModel>
 export type RenamePlaylist = (props: RemovePlaylistModel) => Promise<ResponseModel>
 export type OpenFolderPlaylist = (playlistName: string) => Promise<void>
+
+export type MoveSong = (props: MoveSongModel) => Promise<ResponseModel>
+export type RenameSong = (props: RenameSongModel) => Promise<ResponseModel>
+export type RemoveSong = (props: RemoveSongModel) => Promise<ResponseModel>
 
 export type DownloadSong = (props: DownloadSongModel) => Promise<ResponseModel>
 
