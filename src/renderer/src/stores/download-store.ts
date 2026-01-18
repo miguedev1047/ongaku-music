1c23d2be164402dbbdea86f68d02cc9b3d676ada
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-17 23:04:18 -0500

    CHORE: add playlist select state ⚙️

--- src/renderer/src/stores/download-store.ts
+++ src/renderer/src/stores/download-store.ts
@@ -5,9 +5,11 @@ interface DownloadProps {
   isDownloading: boolean
   progress: VideoProgress | null
   info: VideoInfo | PlaylistInfo | null
+  selectedPlaylist: string
   setIsDownloading: (value: boolean) => void
   setProgress: (value: VideoProgress | null) => void
   setInfoSong: (value: VideoInfo | PlaylistInfo | null) => void
+  selectPlaylist: (value: string) => void
   clearState: () => void
 }
 
@@ -15,7 +17,9 @@ export const useDownloadStore = create<DownloadProps>((set) => ({
   isDownloading: false,
   progress: null,
   info: null,
+  selectedPlaylist: 'none',
   setIsDownloading: (value) => set({ isDownloading: value }),
+  selectPlaylist: (value) => set({ selectedPlaylist: value }),
   setProgress: (value) => set({ progress: value }),
   setInfoSong: (value) => set({ info: value }),
   clearState: () => set({ isDownloading: false, progress: null, info: null })
