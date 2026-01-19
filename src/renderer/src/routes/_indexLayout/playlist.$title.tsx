88d43e389039a4b1aa3532768f6081e493133dad
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-18 22:59:58 -0500

    FEAT: add playlist route skeleton component 🎨

--- src/renderer/src/routes/_indexLayout/playlist.$title.tsx
+++ src/renderer/src/routes/_indexLayout/playlist.$title.tsx
@@ -1,6 +1,10 @@
 import { DownloadInfo } from '@/components/shared/download-info'
 import { SectionContainer } from '@/components/shared/section-container'
-import { SongListHeaderSkeleton, SongListSkeleton } from '@/components/skeletons'
+import {
+  PlaylistRouteSkeleton,
+  SongListHeaderSkeleton,
+  SongListSkeleton
+} from '@/components/skeletons'
 import { SongList, SongListHeader } from '@/components/songs/_index'
 import { playlistSongsQueryOpts } from '@/queries/playlists-queries'
 import { createFileRoute } from '@tanstack/react-router'
@@ -8,6 +12,7 @@ import { Suspense } from 'react'
 
 export const Route = createFileRoute('/_indexLayout/playlist/$title')({
   component: RouteComponent,
+  pendingComponent: PlaylistRouteSkeleton,
   loader: async ({ params, context }) => {
     await context.queryClient.ensureQueryData(playlistSongsQueryOpts(params.title))
     return params
