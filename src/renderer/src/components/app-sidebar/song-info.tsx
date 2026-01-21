c34d5c98d4bfdbef5f7fd71d3b156761327280fe
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-21 10:54:10 -0500

    CHORE: add dynamic color based song picture 🔨

--- src/renderer/src/components/app-sidebar/song-info.tsx
+++ src/renderer/src/components/app-sidebar/song-info.tsx
@@ -1,14 +1,30 @@
+import type { FinalColor } from 'extract-colors/lib/types/Color'
+
 import { useMediaStore } from '@/stores/media-store'
 import { CoverImage } from '@/components/shared/cover-image'
 import { AudioVisualizer } from '@/components/audio-visualizer'
+import { useEffect, useState } from 'react'
+import { extractColors } from 'extract-colors'
 
 export function SongInfo() {
+  const [color, setColor] = useState<FinalColor | null>(null)
   const currentSong = useMediaStore((state) => state.currentSong)
+
+  useEffect(() => {
+    if (!currentSong?.picture) return
+
+    extractColors(currentSong.picture).then((colors) => {
+      setColor(colors[0])
+    })
+  }, [currentSong])
 
   if (!currentSong) return null
 
   return (
-    <div className="w-full h-full p-4 flex flex-col justify-between relative">
+    <div
+      style={{ background: `rgb(${color?.red}, ${color?.green}, ${color?.blue}, 0.10)` }}
+      className="w-full h-full p-4 flex flex-col justify-between relative transition-colors ease-in-out duration-300"
+    >
       <div className="space-y-3">
         <h2 className="uppercase text-lg font-semibold">{currentSong.playlist}</h2>
         <figure className="aspect-square overflow-hidden rounded-lg relative">
