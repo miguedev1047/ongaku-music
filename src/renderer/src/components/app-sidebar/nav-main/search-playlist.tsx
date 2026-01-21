fffe7de39df2e7b7b6b60632449567100cca7943
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-01-21 10:54:43 -0500

    FEAT: add shortcut component on hover 🎨

--- src/renderer/src/components/app-sidebar/nav-main/search-playlist.tsx
+++ src/renderer/src/components/app-sidebar/nav-main/search-playlist.tsx
@@ -2,6 +2,7 @@ import { SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
 import { useDialogStore } from '@/stores/dialog-action-store'
 import { Search } from 'lucide-react'
 import { SearchPlaylist as SearchPlaylistDialog } from '@/components/searchs/_index'
+import { Kbd } from '@/components/ui/kbd'
 
 export function SearchPlaylist() {
   const open = useDialogStore((state) => state.open)
@@ -12,10 +13,13 @@ export function SearchPlaylist() {
 
   return (
     <>
-      <SidebarMenuItem>
+      <SidebarMenuItem className="group/kbd">
         <SidebarMenuButton onClick={handleSearchPlaylist}>
           <Search />
           Search Playlist
+          <div className="flex flex-1 justify-end opacity-0 group-hover/kbd:opacity-100">
+            <Kbd>Ctrl + P</Kbd>
+          </div>
         </SidebarMenuButton>
       </SidebarMenuItem>
       <SearchPlaylistDialog />
